1f6ba9b02064e0d1c78c3bb6db1a7a4c31f6ce04
Slavuta75 <slavuta75@gmail.com>
2018-06-25 16:10:52 +0300

    Make square which fall with gravity

--- UIDynamicAnimator/UIDynamicAnimator/ViewController.swift
+++ UIDynamicAnimator/UIDynamicAnimator/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //UICollisionBehavior - обнаружение столкновений
+    //UIGravityBehavior - обеспечивает тяготение
+    //UIPushBehavior - реагирует на толчки
+    //UISnapBehavior - крепит view к определенной точке
+ 
+    let squareView = UIView()
+    var animator = UIDynamicAnimator()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //создаем квадрат
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        squareView.backgroundColor = UIColor.blue
+        squareView.center = view.center
+        view.addSubview(squareView)
+        
+        //создаем аниматор тяготения
+        animator = UIDynamicAnimator(referenceView: view)
+        let gravity = UIGravityBehavior(items: [squareView])
+        animator.addBehavior(gravity)
+        
+        
     }
 
+ 
 
 }
 
